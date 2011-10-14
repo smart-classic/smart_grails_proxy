26e2ac47d6d13756f22a00b228025f5d2bf5a672
Josh Mandel <Joshua.Mandel@childrens.harvard.edu>
2011-10-14 01:22:37 +0000

    Updated logging config, remove separate stacktrace.log, remove hibernate
    git-svn-id: https://svn.recomdata.com/repo1/chb/smart@9984 155a48cc-27b4-b740-adf7-1490e7b4175d

--- smartproxy/grails-app/conf/Config.groovy
+++ smartproxy/grails-app/conf/Config.groovy
@@ -81,26 +81,12 @@ println grails.config.locations
 
 // log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
 
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+    appenders {
+       'console' name:'stdout'
+       'null' name: 'stacktrace'
+    }
 
-    warn   'org.mortbay.log'
 }
 
 
